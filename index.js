c121d45eaa1d26c474328398740fe9b529b1fd61
haybuka <paschal.nduleo@gmail.com>
2023-01-26 15:11:34 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let harmburger = document.querySelector('.harmburger');
+let navList = document.querySelector('.nav-list');
+
+harmburger.addEventListener('click', () => {
+    // console.log(navList)
+    navList.classList.toggle('toggle')
+})
